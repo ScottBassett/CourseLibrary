c5f73acff6df088a84968deb94424122ba8f6cf7
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-10-28 16:05:05 +0000

    Finished course, implemented PUT, PATCH, DELETE

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 
 namespace CourseLibrary.API
 {
@@ -27,25 +28,30 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers(setupAction =>
-           {
-               setupAction.ReturnHttpNotAcceptable = true;
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
 
-           }).AddXmlDataContractSerializerFormatters()
-               .ConfigureApiBehaviorOptions(setupAction =>
-               {
-                   setupAction.InvalidModelStateResponseFactory = context =>
-                   {
+            }).AddNewtonsoftJson(setupAction =>
+                {
+                    setupAction.SerializerSettings.ContractResolver =
+                        new CamelCasePropertyNamesContractResolver();
+                })
+                .AddXmlDataContractSerializerFormatters()
+                .ConfigureApiBehaviorOptions(setupAction =>
+                {
+                    setupAction.InvalidModelStateResponseFactory = context =>
+                    {
                        // create a problem details object
                        var problemDetailsFactory = context.HttpContext.RequestServices
-                           .GetRequiredService<ProblemDetailsFactory>();
-                       var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
-                           context.HttpContext,
-                           context.ModelState);
+                            .GetRequiredService<ProblemDetailsFactory>();
+                        var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                            context.HttpContext,
+                            context.ModelState);
 
                        // add additional info not added by default
                        problemDetails.Detail = "See the errors field for details.";
-                       problemDetails.Instance = context.HttpContext.Request.Path;
+                        problemDetails.Instance = context.HttpContext.Request.Path;
 
                        // find out which status code to use
                        var actionExecutingContext = context as ActionExecutingContext;
@@ -53,38 +59,38 @@ namespace CourseLibrary.API
                        // if there are modelstate errors & all keys were correctly
                        // found/parsed we're dealing with validation errors
                        if ((context.ModelState.ErrorCount > 0) &&
-                           (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
-                       {
-                           problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
-                           problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
-                           problemDetails.Title = "One or more validation errors occurred.";
+                            (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                        {
+                            problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                            problemDetails.Title = "One or more validation errors occurred.";
 
-                           return new UnprocessableEntityObjectResult(problemDetails)
-                           {
-                               ContentTypes = { "application/problem+json" }
-                           };
-                       }
+                            return new UnprocessableEntityObjectResult(problemDetails)
+                            {
+                                ContentTypes = { "application/problem+json" }
+                            };
+                        }
 
                        // if one of the keys wasn't correctly found / couldn't be parsed
                        // we're dealing with null/unparsable input
                        problemDetails.Status = StatusCodes.Status400BadRequest;
-                       problemDetails.Title = "One or more errors on input occurred.";
-                       return new BadRequestObjectResult(problemDetails)
-                       {
-                           ContentTypes = { "application/problem+json" }
-                       };
-                   };
-               });
+                        problemDetails.Title = "One or more errors on input occurred.";
+                        return new BadRequestObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    };
+                });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-             
-           services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
-           services.AddDbContext<CourseLibraryContext>(options =>
-           {
-               options.UseSqlServer(
-                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
-           }); 
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(
+                     @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
